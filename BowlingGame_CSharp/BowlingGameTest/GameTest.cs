91fe3aa94e04a38def079b406a57d83fc920c976
takaki jun <dyundyun2910@yahoo.co.jp>
2021-11-28 14:32:28 +0900

    TestAllOnesを追加

--- BowlingGame_CSharp/BowlingGameTest/GameTest.cs
+++ BowlingGame_CSharp/BowlingGameTest/GameTest.cs
@@ -38,5 +38,17 @@ namespace BowlingGameTest
 
             Assert.AreEqual(0, game.Score());
         }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            Assert.AreEqual(20, game.Score());
+        }
+
     }
 }
