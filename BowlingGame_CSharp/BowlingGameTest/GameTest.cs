f00d02592a856df38bf44768f7496473c2e51a7c
takaki jun <dyundyun2910@yahoo.co.jp>
2021-11-28 14:59:51 +0900

    Level1_Beginner ブランチのコードを適用

--- BowlingGame_CSharp/BowlingGameTest/GameTest.cs
+++ BowlingGame_CSharp/BowlingGameTest/GameTest.cs
@@ -6,26 +6,104 @@ namespace BowlingGameTest
     [TestClass]
     public class GameTest
     {
-        [TestMethod]
-        public void TestCreate()
+        private Game game;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            new Game();
+            game = new Game();
         }
 
         [TestMethod]
         public void TestOneRoll()
         {
-            var g = new Game();
-            g.Roll(3);
-            Assert.AreEqual(3, g.Score());
+            game.Roll(3);
+            Assert.AreEqual(3, game.Score());
         }
 
         [TestMethod]
         public void TestOneRoll2()
         {
-            var g = new Game();
-            g.Roll(2);
-            Assert.AreEqual(2, g.Score());
+            game.Roll(2);
+            Assert.AreEqual(2, game.Score());
+        }
+
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            MultiRollBySamePins(knockedDownPins: 0, rollCount: 20);
+
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            MultiRollBySamePins(knockedDownPins: 1, rollCount: 20);
+
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);   // Spare
+
+            game.Roll(3);
+            MultiRollBySamePins(knockedDownPins: 0, rollCount: 17);
+
+            Assert.AreEqual(16, game.Score());
+        }
+
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            game.Roll(10);  // Strike
+
+            game.Roll(3);
+            game.Roll(4);
+
+            MultiRollBySamePins(knockedDownPins: 0, rollCount: 16);
+
+            Assert.AreEqual(24, game.Score());
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            MultiRollBySamePins(knockedDownPins: 10, rollCount: 12);
+
+            Assert.AreEqual(300, game.Score());
+        }
+
+        [TestMethod]
+        public void Test10FrameTurkey()
+        {
+            MultiRollBySamePins(knockedDownPins: 0, rollCount: 18);
+            MultiRollBySamePins(knockedDownPins: 10, rollCount: 3);
+
+            Assert.AreEqual(30, game.Score());
+        }
+
+        [TestMethod]
+        public void Test10FrameSpare()
+        {
+            MultiRollBySamePins(knockedDownPins: 0, rollCount: 18);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+
+            Assert.AreEqual(13, game.Score());
+        }
+
+
+        private void MultiRollBySamePins(int knockedDownPins, int rollCount)
+        {
+            for (int i = 0; i < rollCount; i++)
+            {
+                game.Roll(knockedDownPins);
+            }
         }
     }
 }
--- BowlingGame_CSharp/BowlingGame_CSharp/Game.cs
+++ BowlingGame_CSharp/BowlingGame_CSharp/Game.cs
@@ -1,19 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BowlingGame
 {
     public class Game
     {
-        private int score = 0;
+        private List<int> rolls = new List<int>();
+
+        private const int MAX_PINS_IN_FRAME = 10;
+        private const int MAX_FRAMES = 10;
+        private const int MAX_ROLLS_IN_FRAME = 2;
 
         public void Roll(int pins)
         {
-            score += pins;
+            rolls.Add(pins);
+
         }
 
         public int Score()
         {
+            int score = 0;
+
+            int current = 0;
+            for (int frame = 0; frame < MAX_FRAMES; frame++)
+            {
+                score += CalculateFrameScore(current);
+                current = AdjustCurrentRoll(current);
+            }
+
             return score;
         }
+
+        private int AdjustCurrentRoll(int current)
+        {
+            if (IsStrike(current))
+            {
+                current += 1;
+            }
+            else
+            {
+                current += MAX_ROLLS_IN_FRAME;
+            }
+
+            return current;
+        }
+
+        private int CalculateFrameScore(int current)
+        {
+            int score = 0;
+
+            if (IsStrike(current))
+            {
+                score += 10;
+                score += GetStrikeBonus(current);
+            }
+            else if (IsSpare(current))
+            {
+                score += MAX_PINS_IN_FRAME;
+                score += GetSpareBonus(current);
+
+            }
+            else
+            {
+                score += GetCurrentFramePins(current);
+            }
+
+            return score;
+        }
+
+        private int GetCurrentFramePins(int current)
+        {
+            return rolls.Skip(current).Take(2).Sum();
+        }
+
+        private bool IsSpare(int current)
+        {
+            return GetCurrentFramePins(current) == MAX_PINS_IN_FRAME;
+        }
+
+        private bool IsStrike(int current)
+        {
+            return rolls.Skip(current).Take(1).Sum() == MAX_PINS_IN_FRAME;
+        }
+
+        private int GetSpareBonus(int current)
+        {
+            return rolls.Skip(current + 2).Take(1).Sum();
+        }
+
+        private int GetStrikeBonus(int current)
+        {
+            return rolls.Skip(current + 1).Take(2).Sum();
+        }
+
     }
 }
