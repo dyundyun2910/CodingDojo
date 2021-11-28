3769fcd6132f60c0fffa2f8527c22310a959e978
takaki jun <dyundyun2910@yahoo.co.jp>
2021-11-28 14:48:00 +0900

    リファクタ

--- BowlingGame_CSharp/BowlingGame_CSharp/Game.cs
+++ BowlingGame_CSharp/BowlingGame_CSharp/Game.cs
@@ -8,6 +8,10 @@ namespace BowlingGame
     {
         private List<int> rolls = new List<int>();
 
+        private const int MAX_PINS_IN_FRAME = 10;
+        private const int MAX_FRAMES = 10;
+        private const int MAX_ROLLS_IN_FRAME = 2;
+
         public void Roll(int pins)
         {
             rolls.Add(pins);
@@ -19,23 +23,38 @@ namespace BowlingGame
             int score = 0;
 
             int current = 0;
-            for (int frame = 0; frame < 10; frame++)
+            for (int frame = 0; frame < MAX_FRAMES; frame++)
             {
-                if (rolls.Skip(current).Take(2).Sum() == 10)
+                if (IsSpare(current))
                 {
-                    score += 10;
-                    score += rolls.Skip(current + 2).Take(1).Sum();
+                    score += MAX_PINS_IN_FRAME;
+                    score += GetSpareBonus(current);
 
                 }
                 else
                 {
-                    score += rolls.Skip(current).Take(2).Sum();
+                    score += GetCurrentFramePins(current);
                 }
 
-                current += 2;
+                current += MAX_ROLLS_IN_FRAME;
             }
 
             return score;
+        }
+
+        private int GetCurrentFramePins(int current)
+        {
+            return rolls.Skip(current).Take(2).Sum();
+        }
+
+        private bool IsSpare(int current)
+        {
+            return GetCurrentFramePins(current) == 10;
+        }
+
+        private int GetSpareBonus(int current)
+        {
+            return rolls.Skip(current + 2).Take(1).Sum();
         }
     }
 }
