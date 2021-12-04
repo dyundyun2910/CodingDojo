52816baadc229b9d9dea977b32ead68239ebbd6c
takaki jun <dyundyun2910@yahoo.co.jp>
2021-12-04 11:36:03 +0900

    Rollの異常値に対応するため値オブジェクトとしてRollクラスを定義

--- BowlingGame_CSharp/BowlingGameTest/GameTest.cs
+++ BowlingGame_CSharp/BowlingGameTest/GameTest.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BowlingGame;
+using System;
 
 namespace BowlingGameTest
 {
     [TestClass]
     public class GameTest
     {
-        [TestMethod]
-        public void TestCreate()
+        private Game g;
+
+        [TestInitialize]
+        public void Setup()
         {
-            new Game();
+            g = new Game();
         }
+
 
         [TestMethod]
         public void TestOneRoll()
         {
-            var g = new Game();
             g.Roll(3);
             Assert.AreEqual(3, g.Score());
         }
@@ -23,9 +26,23 @@ namespace BowlingGameTest
         [TestMethod]
         public void TestOneRoll2()
         {
-            var g = new Game();
             g.Roll(2);
             Assert.AreEqual(2, g.Score());
+        }
+
+        [TestMethod]
+        public void TestTwoRoll()
+        {
+            g.Roll(2);
+            g.Roll(3);
+            Assert.AreEqual(5, g.Score());
+        }
+
+        [TestMethod]
+        public void TestOneRollOutOfRange()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => g.Roll(-1));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => g.Roll(11));
         }
     }
 }
--- BowlingGame_CSharp/BowlingGame_CSharp/Game.cs
+++ BowlingGame_CSharp/BowlingGame_CSharp/Game.cs
@@ -1,19 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BowlingGame
 {
     public class Game
     {
-        private int score = 0;
+        private List<Roll> rolls;
+
+        public Game()
+        {
+            rolls = new List<Roll>();
+        }
 
         public void Roll(int pins)
         {
-            score += pins;
+            rolls.Add(new Roll(pins));
         }
 
         public int Score()
         {
+            int score = 0;
+            rolls.ForEach(roll => score += roll.ToInt());
             return score;
         }
     }
+
+    internal class Roll
+    {
+        const int MAX_PIN = 10;
+        const int MIN_PIN = 0;
+
+        private int pins;
+
+        public Roll(int pins)
+        {
+            if (pins < MIN_PIN) throw new ArgumentOutOfRangeException();
+            if (pins > MAX_PIN) throw new ArgumentOutOfRangeException();
+
+            this.pins = pins;
+        }
+
+        public int ToInt()
+        {
+            return pins;
+        }
+    }
+
 }
