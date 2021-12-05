8b34832fa2ec591787db700c4040b11beb635dd9
takaki jun <dyundyun2910@yahoo.co.jp>
2021-12-05 18:06:58 +0900

    Rust版の初期コード

---
+++ Rust/Level1_Beginner/bowling_game/ReadMe.md
@@ -0,0 +1,17 @@
+# Rust言語版の bowling_game Kata
+
+## How to build
+
+Windows環境でのRustのSetup
+
+https://docs.microsoft.com/ja-jp/windows/dev-environment/rust/setup
+
+環境がセットアップできたら、Rustの通常どおり cargo を使う
+
+```
+# ビルド
+cargo build
+
+# ユニットテスト実行
+cargo test
+```
---
+++ Rust/Level1_Beginner/bowling_game/src/lib.rs
@@ -0,0 +1,37 @@
+#[allow(dead_code)]
+struct Game {
+    score: u32,
+}
+
+#[allow(dead_code)]
+impl Game {
+    fn roll(&mut self, pins: u32) {
+        self.score += pins;
+    }
+
+    fn score(&self) -> u32 {
+        return self.score;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_roll() {
+        let mut game = Game { score: 0 };
+        game.roll(3);
+
+        assert_eq!(3, game.score());
+    }
+
+    #[test]
+    fn two_roll() {
+        let mut game = Game { score: 0 };
+        game.roll(1);
+        game.roll(2);
+
+        assert_eq!(3, game.score());
+    }
+}
